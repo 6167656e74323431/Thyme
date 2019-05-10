c7ebe89bee8ca0f226095f9501368aedcfbe6cd8
Larry <ehhthing@users.noreply.github.com>
2019-05-10 14:14:47 -0400

    Add game boilerplate.

---
+++ desktop/src/ml/educationallydesigned/thyme/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ml.educationallydesigned.thyme.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ml.educationallydesigned.thyme.Thyme;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Thyme(), config);
+	}
+}
