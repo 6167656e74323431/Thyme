84b6a436831284f4d9b54567c8704a339858967e
ehhthing <ehhthing@users.noreply.github.com>
2019-05-27 00:02:07 -0400

    Fix a broken revert

--- core/src/ml/educationallydesigned/thyme/Thyme.java
+++ core/src/ml/educationallydesigned/thyme/Thyme.java
@@ -1,33 +1,31 @@
+/*
+	Thyme is an educational game to assist teenagers in time management, and tracking.
+	Copyright (C) 2019 Theodore Preduta, Larry Yuan
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package ml.educationallydesigned.thyme;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.kotcrab.vis.ui.VisUI;
+import ml.educationallydesigned.thyme.core.HomeScreen;
 
-public class Thyme extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-
+public class Thyme extends Game {
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		VisUI.load(VisUI.SkinScale.X2);
+		this.setScreen(new HomeScreen(this));
 	}
 }
